a5c0448e304f85bb3561d9770f1e2e83c4b1e5dd
MalshaNanayakkara <malshananayakkara20@gmail.com>
2020-04-18 14:46:52 +0530

    created insert method in java class

--- HospitalDetailsManagement/src/model/Hospital.java
+++ HospitalDetailsManagement/src/model/Hospital.java
@@ -28,7 +28,40 @@ public class Hospital {
 	
 	}
 	
+	public String insertHospitals(String hName, String hProvince, String hDistrict, String hEmail,String hPhone,String hServices) 
+	{
+			
+		String output = ""; 
 		
+		try {
+		Connection con = connect(); 
+		 
+		if (con == null) { 
+			return "Error while connecting to the database"; 
+		} 
+	
+	
+		String query = " insert into hospital (hospitalID,hospitalName,hospitalProvince,hospitalDistrict,hospitalEmail,hospitalPhone,hospitalServices)" + " values (?,?,?,?,?,?,?)"; 
+		java.sql.PreparedStatement preparedStmt = con.prepareStatement(query);
+		
+		preparedStmt.setInt(1, 0);
+		preparedStmt.setString(2, hName);
+		preparedStmt.setString(3, hProvince);
+		preparedStmt.setString(4, hDistrict);
+		preparedStmt.setString(5, hEmail);
+		preparedStmt.setString(6, hPhone);
+		preparedStmt.setString(7, hServices);
+		preparedStmt.execute();
+		con.close(); 
+		output = "Inserted successfully"; 
+		
+		}
+		catch(Exception e) {
+		output = "Error while inserting";  
+		System.err.println(e.getMessage()); 
+		}
+		return output; 
+	}	
 }
 	
 	
