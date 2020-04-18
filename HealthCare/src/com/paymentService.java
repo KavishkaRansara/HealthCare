1b48f65247929f56bec955bf8c088787c16b74a3
Kosasip(M)Pasapukal <kosasip4pasapukal@gmail.com>
2020-04-18 06:38:28 -0700

    implementation of the read and insert payment services have done with client side tesing in postmanCanary

--- HealthCare/src/com/paymentService.java
+++ HealthCare/src/com/paymentService.java
@@ -1,6 +1,12 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -13,11 +19,24 @@ public class paymentService {
     @GET
     @Path("/")
     @Produces(MediaType.TEXT_HTML)
-    public String readPayment() {
-    	String msg = "";
-    	if(pay.getconnection() !=  null) {
-    		msg = "success";
-    	}
-    	return msg;
-    } 
+	public String readRemainpayments()
+	{
+    	return pay.readRemainpayment();
+		  
+	}
+    
+
+	//POST METHOD
+	@POST 
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertPayment(@FormParam("patientID") String patientID,
+							@FormParam("doctorID") String doctorID,
+							@FormParam("amount") String amount)
+							
+	{
+		String output = pay.appointmentAmount(patientID, doctorID, amount);  
+		return output; 
+	}
 }
--- HealthCare/src/model/payment.java
+++ HealthCare/src/model/payment.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 
 public class payment {
 
@@ -20,5 +21,110 @@ public class payment {
 		  return con;
 	  }
 	  
+//insert payment, while they paid the deletion will happen with this(the payment will delete from remain payment table)  
+	  public String insertPayment() {
+		  String msg = "";
+		  
+		  return msg;
+	  }
 	  
+//Add payment to the appointment
+	  public String appointmentAmount(String patientID,String doctorID,String amount) {
+	      String msg = "";
+		  
+	      try {
+				Connection con = getconnection();
+
+				if (con == null)
+
+				{
+					return "Error while connecting to the database for inserting appointment details.";
+				}
+				// create a prepared statement
+				String query = " insert into remainpayment (payID,patientID,doctorID,amount)" + " values (?,?,?,?)"; 
+				java.sql.PreparedStatement preparedStmt = con.prepareStatement(query);
+				
+				// binding values to appointment table
+				preparedStmt.setInt(1, 0);
+				preparedStmt.setString(2, patientID);
+				preparedStmt.setString(3, doctorID);
+				preparedStmt.setDouble(4, Double.parseDouble(amount));
+				
+				preparedStmt.execute();
+				con.close();
+				msg = "Inserted successfully"; 
+
+				
+				} 
+				catch (Exception e) {
+				msg = "Error while inserting the appointment details.";
+				System.err.println(e.getMessage());
+				}
+	      
+		  return msg;
+	  }
+	  
+//update added amount
+	  public String updateAmount() {
+	      String msg = "";
+		  
+		  return msg;
+	  } 
+
+	  //reading the payment details
+	  public String readRemainpayment() {
+		  String output="";
+			try {
+  
+				
+				Connection con = getconnection();
+
+				if (con == null) {
+
+					return "Error while connecting to the database for reading the appointmentdetails.";
+
+				}
+				output = "<table border=\"1\"><tr>"
+						+ "<th>Payment ID</th>"
+						+ "<th>patient ID</th>"
+						+ "<th>doctor ID</th>"
+						+ "<th>Amount</th></tr>"; 
+
+				String query = "select * from remainpayment";
+
+				java.sql.Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+
+				while (rs.next()) {
+
+					String payID = Integer.toString(rs.getInt("payID"));
+					String patientID = rs.getString("patientID");
+					String doctorID = rs.getString("doctorID");
+					String amount = Double.toString(rs.getDouble("amount"));
+					
+
+					output += "<td>" + payID + "</td>";
+					output += "<td>" + patientID + "</td>";   
+					output += "<td>" + doctorID + "</td>"; 
+					output += "<td>" + amount + "</td>";
+					    
+
+				}
+
+				con.close();
+
+				output += "</table>";
+
+				
+			} catch (Exception e) {
+
+				output = "An error occurred while reading the appointment details. ";
+				System.err.println(e.getMessage());
+
+			}
+
+			return output;
+
+	  }
+
 }
