a456b925c6d799d83200b161911425bbfa1bb426
MalshaNanayakkara <malshananayakkara20@gmail.com>
2020-04-18 14:59:32 +0530

    Created methos for reading hospitals

--- HospitalDetailsManagement/src/model/Hospital.java
+++ HospitalDetailsManagement/src/model/Hospital.java
@@ -61,7 +61,72 @@ public class Hospital {
 		System.err.println(e.getMessage()); 
 		}
 		return output; 
-	}	
+	}
+	
+	public String readHospitals() 
+	{
+		String output = "";
+		
+		try {
+		Connection con = connect(); 
+			 
+			if (con == null) { 
+				return "Error while connecting to the database for reading."; 
+			} 
+			output = "<table border=\"1\"><tr>"
+					+ "<th>Hospital ID</th>"
+					+ "<th>Hospital Name</th>"
+					+ "<th>Hospital Province</th>"
+					+ "<th>Hospital District</th>"
+					+ "<th>Email</th>"
+					+ "<th>Phone</th>"
+					+ "<th>Services</th>"
+					+ "<th>Update</th>"
+					+ "<th>Remove</th></tr>"; 
+			 
+			String query = "select * from hospital"; 
+			java.sql.Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			while (rs.next()) 
+			{   
+				String hospitalID = Integer.toString(rs.getInt("hospitalID")); 
+				String hospitalName = rs.getString("hospitalName");  
+				String hospitalProvince = rs.getString("hospitalProvince"); 
+				String hospitalDistrict = rs.getString("hospitalDistrict");
+				String hospitalEmail = rs.getString("hospitalEmail");
+				String hospitalPhone = rs.getString("hospitalPhone");
+				String hospitalServices = rs.getString("hospitalServices");
+				
+				output += "<tr><td>" + hospitalID + "</td>";
+				output += "<td>" + hospitalName + "</td>";
+				output += "<td>" + hospitalProvince + "</td>";   
+				output += "<td>" + hospitalDistrict + "</td>"; 
+				output += "<td>" + hospitalEmail + "</td>";
+				output += "<td>" + hospitalPhone + "</td>";
+				output += "<td>" + hospitalServices + "</td>";
+				
+				
+				output += "<td><input name=\"btnUpdate\" "  
+					   + " type=\"button\" value=\"Update\"></td>"   
+					   + "<td><form method=\"post\" action=\"hospitals.jsp\">"
+				       + "<input name=\"btnRemove\" "
+					   + " type=\"submit\" value=\"Remove\" class=\"btn btn-danger\">"
+				       + "<input name=\"hospitalID\" type=\"hidden\" "   
+					   + " value=\"" + hospitalID + "\">" 
+				       + "</form></td></tr>";    
+			 
+			}
+			con.close(); 
+			output += "</table>";  
+		
+		}
+		catch (Exception e) {  
+			output = "Error while reading the hospitals.";  
+			System.err.println(e.getMessage()); 
+		}
+		return output;
+	}
 }
 	
 	
