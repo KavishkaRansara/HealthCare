2cfdae608d9b8b9a84e3e570b737583dedd7e347
Dulashni <dulashni@gmail.com>
2020-04-18 17:57:38 +0530

    View Doctors Method Added

--- DoctorManage/src/model/Doctor.java
+++ DoctorManage/src/model/Doctor.java
@@ -26,6 +26,63 @@ public class Doctor {
 	}
 	
 	
+	
+	public String readDoctor(){
+
+		String output = "";
+		try
+		{
+				Connection con = connect();
+				if (con == null)
+				{
+					return "Error while connecting to the database for reading."; 
+				}
+
+
+				// Prepare the html table to be displayed
+				output = "<table border=\"1\"><tr><th>Doctor Name</th><th>Specialization</th><th>Hospital</th><th>Contact</th><th>Email</th><th>Status</th><th>Update</th><th>Remove</th></tr>";
+				String query = "select * from doctors";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+				// iterate through the rows in the result set
+				while (rs.next())
+				{
+					String docID = Integer.toString(rs.getInt("docID"));
+					String docName = rs.getString("docName");
+					String docSpec = rs.getString("docSpec");
+					String docHosp = rs.getString("docHosp");
+					String docContact = Integer.toString(rs.getInt("docContact"));
+					String docEmail = rs.getString("docEmail");
+					String docStatus = rs.getString("docStat");
+
+					// Add into the html table
+					output += "<tr><td>" + docName + "</td>";
+					output += "<td>" + docSpec + "</td>";
+					output += "<td>" + docHosp + "</td>";
+					output += "<td>" + docContact + "</td>";
+					output += "<td>" + docEmail + "</td>";
+					output += "<td>" + docStatus + "</td>";
+					
+					// buttons
+					output += "<td><input name=\"btnUpdate\" type=\"button\" value=\"Update\" class=\"btn btn-secondary\"></td>"
+					+ "<td><form method=\"post\" action=\"index.jsp\">"
+					+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\" class=\"btn btn-danger\">"
+					+ "<input name=\"itemID\" type=\"hidden\" value=\"" + docID
+					+ "\">" + "</form></td></tr>";
+				}
+
+				con.close();
+				// Complete the html table
+				output += "</table>";
+		}
+		catch (Exception e)
+		{
+				output = "Error while reading the values.";
+				System.err.println(e.getMessage());
+		}
+	return output;
+}
+	
 }	
 
 
