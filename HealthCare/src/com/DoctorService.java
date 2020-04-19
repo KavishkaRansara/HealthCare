f96c903215bc2365fa2d8d1e7eb608ed225b5852
Kosasip(M)Pasapukal <kosasip4pasapukal@gmail.com>
2020-04-19 04:27:51 -0700

    successfully integrate Doctor service implementaion, web.xml was edited for servlet ordering

---
+++ HealthCare/src/com/DoctorService.java
@@ -0,0 +1,96 @@
+package com;
+
+import model.Doctor;
+
+//for REST service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//for JSON
+import com.google.gson.*;
+
+//for XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+
+@Path("/Doctor")
+
+public class DoctorService {
+	
+	Doctor docObj = new Doctor();
+	
+	//for viewing doctors
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readDoctor() {
+		
+		return docObj.readDoctor();
+		
+	}
+	
+	
+	//for inserting doctors	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertDoctor(   @FormParam("docName") String doctorName,
+								  @FormParam("docSpec") String specialization,
+								  @FormParam("docHosp") String hospital,
+								  @FormParam("docContact") String contact,
+								  @FormParam("docEmail") String email,
+								  @FormParam("docStatus") String status)
+	{
+			String output = docObj.insertDoctor(doctorName, specialization, hospital, contact, email, status);
+			return output;
+	}
+	
+	//insert and view is tested using POSTMAN
+	
+	
+	//for updating doctors
+	   @PUT
+	   @Path("/")
+	   @Produces(MediaType.TEXT_PLAIN)
+	   public String updateDoctor(String docData) {
+		
+		 //Convert the input string to a JSON object
+		  JsonObject docjObj = new JsonParser().parse(docData).getAsJsonObject();
+		  
+		//Read the values from the JSON object
+		  String doctorID= docjObj.get("docID").getAsString();
+		  String doctorName= docjObj.get("docName").getAsString();
+		  String specialization= docjObj.get("docSpec").getAsString();
+		  String hospital= docjObj.get("docHosp").getAsString();
+		  String contact = docjObj.get("docContact").getAsString();
+		  String email= docjObj.get("docEmail").getAsString();
+		  String status= docjObj.get("docStatus").getAsString();
+		  
+		  String output= docObj.updateDoctor(doctorID, doctorName,specialization,hospital,contact,email,status);
+		  return output;
+		  
+		  
+		 
+	  }
+	   
+	   
+	   //for deleting doctor
+	   @DELETE
+	   @Path("/")
+	   @Consumes(MediaType.APPLICATION_XML)
+	   @Produces(MediaType.TEXT_PLAIN)
+	   public String deleteDoctor(String docData)
+	   {
+	   			//Convert the input string to an XML document
+	   			Document doc = Jsoup.parse(docData, "", Parser.xmlParser());
+	   			//Read the value from the element <itemID>
+	   			String doctorID = doc.select("docID").text();
+	   			String output = docObj.deleteDoctor(doctorID);
+	   			return output;
+	   }
+
+}
---
+++ HealthCare/src/model/Doctor.java
@@ -0,0 +1,207 @@
+package model;
+
+import java.sql.*;
+
+
+public class Doctor {
+	
+	//connection the code to the mysql db
+	
+	public Connection connect(){
+		Connection con = null;
+	 try
+	 {
+	       Class.forName("com.mysql.jdbc.Driver");
+	       //Provide the correct details: DBServer/DBName, username, password
+	       con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/healthcare","root", "Sliitstudent123");
+	       
+	       //For testing
+	       System.out.print("Successfully connected");
+	 }
+	 catch(Exception e){
+
+	    e.printStackTrace();
+	 }
+	 return con;
+	}
+	
+	
+public String insertDoctor(String doctorName, String specialization, String hospital, String contact, String email, String status){
+	    
+	    String output = "";
+		
+		try{
+	         Connection con = connect();
+	         if (con == null)
+	         {
+	             return "Error while connecting to the database";
+	         }
+	        
+	        // create a prepared statement
+	        String query = " insert into doctors(`docID`,`docName`,`docSpec`,`docHosp`,`docContact`,`docEmail`,`docStat`)" + " values (?, ?, ?, ?, ?, ?, ?)";
+	        PreparedStatement preparedStmt = con.prepareStatement(query);
+
+	        // binding values
+	        preparedStmt.setInt(1, 0);
+	        preparedStmt.setString(2, doctorName);
+	        preparedStmt.setString(3, specialization);
+	        preparedStmt.setString(4, hospital);
+	        preparedStmt.setInt(5, Integer.parseInt(contact));
+	        preparedStmt.setString(6, email);
+	        preparedStmt.setString(7, status);
+
+	        //execute the statement
+	        preparedStmt.execute();
+	        con.close();
+
+	        output = "Inserted successfully";
+
+		}catch(Exception e){
+
+	        output = "Error while inserting";
+	        System.err.println(e.getMessage());
+		}
+
+		return output;
+
+	}
+	
+	
+	
+	public String readDoctor(){
+
+		String output = "";
+		try
+		{
+				Connection con = connect();
+				if (con == null)
+				{
+					return "Error while connecting to the database for reading."; 
+				}
+
+
+				// Prepare the html table to be displayed
+				output = "<table border=\"1\"><tr><th>Doctor Name</th><th>Specialization</th><th>Hospital</th><th>Contact</th><th>Email</th><th>Status</th><th>Update</th><th>Remove</th></tr>";
+				String query = "select * from doctors";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+				// iterate through the rows in the result set
+				while (rs.next())
+				{
+					String docID = Integer.toString(rs.getInt("docID"));
+					String docName = rs.getString("docName");
+					String docSpec = rs.getString("docSpec");
+					String docHosp = rs.getString("docHosp");
+					String docContact = Integer.toString(rs.getInt("docContact"));
+					String docEmail = rs.getString("docEmail");
+					String docStatus = rs.getString("docStat");
+
+					// Add into the html table
+					output += "<tr><td>" + docName + "</td>";
+					output += "<td>" + docSpec + "</td>";
+					output += "<td>" + docHosp + "</td>";
+					output += "<td>" + docContact + "</td>";
+					output += "<td>" + docEmail + "</td>";
+					output += "<td>" + docStatus + "</td>";
+					
+					// buttons
+					output += "<td><input name=\"btnUpdate\" type=\"button\" value=\"Update\" class=\"btn btn-secondary\"></td>"
+					+ "<td><form method=\"post\" action=\"index.jsp\">"
+					+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\" class=\"btn btn-danger\">"
+					+ "<input name=\"itemID\" type=\"hidden\" value=\"" + docID
+					+ "\">" + "</form></td></tr>";
+				}
+
+				con.close();
+				// Complete the html table
+				output += "</table>";
+		}
+		catch (Exception e)
+		{
+				output = "Error while reading the values.";
+				System.err.println(e.getMessage());
+		}
+	return output;
+}
+	
+	
+	
+	
+	public String updateDoctor(String doctorID, String doctorName, String specialization, String hospital, String contact, String email, String status){
+	    
+	    String output = "";
+
+	    try{
+
+	           Connection con = connect();
+	           if (con == null){
+	           return "Error while connecting to the database for updating.";
+	           }
+	           
+	           // create a prepared statement
+
+	           String query = "UPDATE doctors SET docName=?,docSpec=?,docHosp=?,docContact=?,docEmail=?,docStat=? WHERE docID=?";
+	           PreparedStatement preparedStmt = con.prepareStatement(query);
+
+	           preparedStmt.setString(1, doctorName);
+	           preparedStmt.setString(2, specialization);
+	           preparedStmt.setString(3, hospital);
+	           preparedStmt.setInt(4, Integer.parseInt(contact));
+	           preparedStmt.setString(5, email);
+	           preparedStmt.setString(6, status);
+	           preparedStmt.setInt(7, Integer.parseInt(doctorID));
+
+	           // execute the statement
+	           preparedStmt.execute();
+	           con.close();
+
+	           output = "Updated successfully";
+	           
+
+
+	        }catch(Exception e){
+	        	output = "Error while updating the value.";
+				System.err.println(e.getMessage());
+	        }
+
+	        return output;
+
+	    }
+	
+	
+	public String deleteDoctor(String doctorID){
+
+		String output = "";
+
+		try{
+
+			Connection con = connect();
+			if (con == null){
+
+	         return "Error while connecting to the database for deleting.";
+	        }
+
+	        // create a prepared statement
+	        String query = "delete from doctors where docID=?";
+	        PreparedStatement preparedStmt = con.prepareStatement(query);
+	        // binding values
+	        preparedStmt.setInt(1, Integer.parseInt(doctorID));
+	        // execute the statement
+	        preparedStmt.execute();
+	        con.close();
+
+	        output = "Deleted successfully";
+
+
+		}catch (Exception e){
+
+			output = "Error while deleting the value.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+	
+	
+	
+}
